8039833700521cd27dee5d5061e6040a33e14610
wangjianjun <1176398319@qq.com>
2020-12-05 12:55:08 +0800

    添加版权声明

--- src/Arctic.Books/Book.cs
+++ src/Arctic.Books/Book.cs
@@ -1,3 +1,17 @@
+// Copyright 2020 王建军
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 using Arctic.Auditing;
 using System;
 using System.ComponentModel.DataAnnotations;
@@ -26,7 +40,7 @@ namespace Arctic.Books
         public virtual string Author { get; set; }
 
         /// <summary>
-        /// 值类型天然不可为 null。
+        /// 值类型不需要加 Required。
         /// </summary>
         public virtual DateTime PublicationDate { get; set; }
 
@@ -34,10 +48,11 @@ namespace Arctic.Books
 
         #region auditing properties
 
-        #endregion
         public virtual string cuser { get; set; }
         public virtual string muser { get; set; }
         public virtual DateTime mtime { get; set; }
         public virtual DateTime ctime { get; set; }
+
+        #endregion
     }
 }
